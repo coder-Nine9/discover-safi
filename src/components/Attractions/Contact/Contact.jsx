2f0abb2b68ed0ec386abc6a0f2d2642b07ac699b
abdelaziz <workercode25@gmail.com>
2026-07-15 16:02:09 +0100

    finish contact section

--- src/components/Attractions/Contact/Contact.jsx
+++ src/components/Attractions/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import Form from "./Form";
 export default function Contact() {
   return (
     <section className=" py-30">
-      <div className="flex flex-wrap  gap-5 px-8 w-[81.5%] m-auto">
+      <div className="mx-auto flex max-w-7xl items-start gap-14 px-8">
         <Info />
         <Form />
       </div>
--- src/components/Attractions/Contact/Form.jsx
+++ src/components/Attractions/Contact/Form.jsx
@@ -1,33 +1,43 @@
 export default function Form() {
   return (
-    <div>
-      <div className="grid  grid-rows-3">
+    <form className="flex-1">
+      <div className="grid grid-cols-2 gap-x-5 gap-y-6">
         <Field label="Full name" placeHolder="Your name" />
-        <Field label="Email" placeHolder="you@gmail.com" />
+
+        <Field label="Email" placeHolder="you@gmail.com" type="Email" />
+
         <Field label="Subject" placeHolder="What's this about?" />
-        <div className="flex flex-col gap-1 mb-5">
+
+        <div className=" col-span-2 flex flex-col gap-2  mb-5">
           <label
             htmlFor=""
             className="font-bold text-[12.5px] mb-2 text-[#0B2E42] font-[Inter,sans-serif]"
           >
             Message
           </label>
+
           <textarea
             className="py-3.5 px-4 bg-[#F7F1E3] rounded-2xl h-28.25"
             placeholder="Tell us about your trip..."
           />
         </div>
-        <button className="p-[16px_30px] rounded-full bg-[#0B2E42] text-[14.5px] font-[Arial] font-bold text-white">
+      </div>
+
+      <div className="col-span-2">
+        <button className="cursor-pointer p-[16px_30px] rounded-full bg-[#0B2E42] text-[14.5px] font-[Arial] font-bold text-white transition-all duration-300 ease  hover:-translate-y-1.25">
           Send Message
         </button>
       </div>
-    </div>
+    </form>
   );
 }
 
-function Field({ label, placeHolder }) {
+function Field({ label, placeHolder, type = "text" }) {
   return (
-    <div className="flex flex-col gap-1 mb-5">
+    <div
+      className="flex flex-col gap-1 mb-5 "
+      style={{ gridColumn: label === "Subject" ? "span 2 / span 2" : null }}
+    >
       <label
         htmlFor=""
         className="font-bold text-[12.5px] mb-2 text-[#0B2E42] font-[Inter,sans-serif]"
@@ -35,7 +45,7 @@ function Field({ label, placeHolder }) {
         {label}
       </label>
       <input
-        type="textarea"
+        type={type}
         placeholder={placeHolder}
         className="py-3.5 px-4 bg-[#F7F1E3] rounded-2xl"
       />
